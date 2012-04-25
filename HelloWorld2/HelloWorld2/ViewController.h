aafe806491d590b40e1992e00c575eaf1b348a7d
liangcm <liangchuanmin@gmail.com>
2012-04-26 00:34:59 +0800

    Why I can't commit?Just a test.

---
+++ HelloWorld2/HelloWorld2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorld2
+//
+//  Created by raymond on 12-4-26.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
