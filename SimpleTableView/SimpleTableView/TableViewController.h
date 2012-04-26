dd2b5920894a1623957778e7df8febd0a08cdb95
liangcm <liangchuanmin@gmail.com>
2012-04-26 21:52:33 +0800

    add TableViewController.

---
+++ SimpleTableView/SimpleTableView/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  SimpleTableView
+//
+//  Created by raymond on 12-4-26.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
